5a09847f68a5978737d59d59ad246b72569512a2
Rami ALZEBAK <r.alzebak@gmail.com>
2022-02-13 18:45:22 +0100

    register

--- src/server/app.py
+++ src/server/app.py
@@ -1,11 +1,14 @@
 from copy import copy
 from crypt import methods
+import datetime
 from telnetlib import STATUS
 from flask import Flask, request, make_response, jsonify
 import uuid
 import json
 from flask_cors import CORS
+import base64
 
+from itsdangerous import base64_decode
 
 users = [
     {
@@ -46,10 +49,32 @@ def find_session(id):
     return None
 
 
+@app.route('/sessions/', methods=['GET'])
+def getSessions():
+    return make_response(jsonify(sessions), 200)
+
+
+@app.route('/events/<session_id>', methods=['GET'])
+def getEvents(session_id):
+    return make_response(jsonify(filter(lambda x: x['session_id'] == session_id, events)), 200)
+
+
 @app.route('/register/<session_id>', methods=['POST'])
 def register(session_id):
     body = request.get_json()
+    body["session_id"] = session_id
+    if 'photo' in body.keys():
+        photo = body['photo'][22:]
+        imagePath = "static/generated/image-{0}-{1}.jpg".format(
+            session_id, len(events))
 
+        with open(imagePath, "wb") as fh:
+            fh.write(base64.urlsafe_b64decode(photo))
+        del body["photo"]
+        body["image"] = request.host_url + imagePath
+
+    events.append(body)
+    print(events)
     return make_response('OK', 200)
 
 
@@ -70,12 +95,15 @@ def login():
     session['NE'] = user['NE']
     session['started'] = False
     session['camera_in_view'] = False
+    session['start-date'] = None
+    session['end-date'] = None
+    session['ended'] = False
     sessions.append(session)
 
     return make_response(jsonify(session), 200)
 
 
-@app.route('/start/<session_id>' , methods=['POST'])
+@app.route('/start/<session_id>', methods=['POST'])
 def start(session_id):
 
     session = find_session(session_id)
@@ -84,4 +112,18 @@ def start(session_id):
         return make_response('Camera not in view', 200)
 
     session['started'] = True
+    session['start-date'] = datetime.datetime.now()
+    return make_response('OK', 200)
+
+
+@app.route('/end/<session_id>', methods=['POST'])
+def end(session_id):
+
+    session = find_session(session_id)
+
+    if session is None:
+        return make_response('Session not found', 404)
+
+    session['ended'] = True
+    session['end-date'] = datetime.datetime.now()
     return make_response('OK', 200)
